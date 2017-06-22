b3818dfec6665c75e1acf0561bf1b1610c9c22b7
elfogaca <elfogaca@gmail.com>
2017-06-22 19:48:35 -0300

    Initial commit

---
+++ app/src/main/java/br_elfo/edmarluizleonefogaca/floatmenu/MainActivity.java
@@ -0,0 +1,71 @@
+package br_elfo.edmarluizleonefogaca.floatmenu;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.app.AlertDialog;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        setContentView(R.layout.activity_main);
+
+        fab = (FloatingActionButton) findViewById(R.id.fabBottom1);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Activity2.class);
+                startActivity(i);
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.about) {
+            TextView content = (TextView) getLayoutInflater().inflate(R.layout.about_view, null);
+            content.setMovementMethod(LinkMovementMethod.getInstance());
+            content.setText(Html.fromHtml(getString(R.string.about_body)));
+            new AlertDialog.Builder(this)
+                    .setTitle(R.string.about)
+                    .setView(content)
+                    .setInverseBackgroundForced(true)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    }).create().show();
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+}
